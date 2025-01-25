52937bda94cfca733c97da6bcfcdd78c0c9a43d5
LucasDiabloChan <bomdiao53@gmail.com>
2025-01-25 16:53:18 -0300

    Created an endpoint (search by game id) and added GameDTO

--- src/main/java/com/aprendizado/ds_list/services/GameService.java
+++ src/main/java/com/aprendizado/ds_list/services/GameService.java
@@ -3,21 +3,29 @@ package com.aprendizado.ds_list.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.aprendizado.ds_list.dto.GameDTO;
 import com.aprendizado.ds_list.dto.GameMinDTO;
 import com.aprendizado.ds_list.entities.Game;
 import com.aprendizado.ds_list.repositories.GameRepository;
 
+
 // Both can be used to identify this class as an component
 @Service
-@Component
 public class GameService {
 
 	@Autowired
 	private GameRepository gameRepository;
 
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		return new GameDTO(result);
+	}   
+	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll() {
 		List<Game> result = gameRepository.findAll();
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
