5aaba7d5d90e9c918b53d260358e6067b60611a9
LucasDiabloChan <bomdiao53@gmail.com>
2025-01-26 19:30:38 -0300

    Home page created

--- src/main/java/com/aprendizado/ds_list/controllers/NavigationController.java
+++ src/main/java/com/aprendizado/ds_list/controllers/NavigationController.java
@@ -14,9 +14,28 @@ public class NavigationController {
 				Take a look on the return of endpoints:
 				<ul>
 					<li>
-					 	<a href="localhost:8080/games"> See all games </a>
+					 	<a href="/"> This page (home) </a>
+					</li>
+					<li>
+					 	<a href="/lists"> See all games genres </a>
+					</li>
+					<li>
+					 	<a href="/lists/1/games"> See all games of an especific genre </a>
+					 	<br>
+					 	(1 - Adventure / 2 - Platform)
+					</li>
+					<li>
+					 	<a href="/lists/1/replacement"> Change the position of a game in the list (this actually doesn't work because it needs a request body)</a>
+					</li>
+					<li>
+					 	<a href="/games"> See all games (yes, again) </a>
+					</li>
+					<li>
+					 	<a href="/games/2"> See an especific game </a>
 					</li>
 				</ul>
+				
+				<p>Obs: you can edit the numbers in the URL to change the return :) </p>
 				""";
 	}
 }
